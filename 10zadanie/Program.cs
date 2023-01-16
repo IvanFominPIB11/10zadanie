d669d899adb28e9de26054faa09d3477facca16d
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-16 15:07:25 +0700

    Добавьте файлы проекта.

---
+++ 10zadanie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10zadanie
+{
+    class zadanie10
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ввести первое число");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ввести второе число");
+            var b = int.Parse(Console.ReadLine());
+            Console.WriteLine( a + b );
+            Console.WriteLine( a - b );
+            Console.ReadKey();
+        }
+    }
+}
